db13c15f2cfb6aeba5f0411fb2019413ce04fc10
Vamshika03 <vamshika.lothumalla@gmail.com>
2025-03-27 11:25:44 +0530

    Update Rows.py

--- Week2/Rows.py
+++ Week2/Rows.py
@@ -1,9 +1,5 @@
 
-Q2)Printing of numbers:
-A)
 for i in range(1,6):
-    print(str(i)*i)
-
-B)
-for i in range(5,0,-1):
-    print(str(i)*i)
+   for j in range(i+1):
+       print(i)
+       print()
