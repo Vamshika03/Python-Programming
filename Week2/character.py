8feb1f91c3e1b81376df10f015a26b25a157cefa
Vamshika03 <vamshika.lothumalla@gmail.com>
2025-03-28 15:11:37 +0530

    Create character.py

---
+++ Week2/character.py
@@ -0,0 +1,11 @@
+Q)Character Checking:
+
+x=input("Enter")
+if x .islower():
+    print("Lower")
+elif x .isupper():
+    print("Upper")
+elif x .isdigit():    
+    print("Digit")
+else:
+    print("Special")
