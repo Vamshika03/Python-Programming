a07e5bb105724b303e0071979776a27be0824664
Vamshika03 <vamshika.lothumalla@gmail.com>
2025-04-03 12:02:20 +0530

    Create bank.py

---
+++ Week3/bank.py
@@ -0,0 +1,27 @@
+Q)Bank Application
+
+def credit(balance,Balance):
+    
+    Balance=Balance+balance
+    print(Balance)
+    return Balance
+
+def debit(balance,Balance):
+    Balance=Balance-balance
+    print(Balance)  
+    return Balance
+    
+def checkbalance(Balance):
+    print(Balance)
+    
+    
+    
+    
+    
+    
+    
+    
+Balance=0
+Balance=credit(1000,Balance)
+Balance=debit(500,Balance)
+Balance=checkbalance(500)
